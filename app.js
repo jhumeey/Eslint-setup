e8b66015075218933e689c20b25d7d9c27cf72fe
giselle2323 <yakubuaminat94@gmail.com>
2020-04-03 13:05:24 +0100

    adding eslint to a project

---
+++ app.js
@@ -0,0 +1,18 @@
+function addNums(x, y) {
+  return x + y;
+}
+const people = [
+  'bill',
+  'bill',
+  'sed',
+];
+
+const sayHi = () => {
+  'yes';
+};
+addNums();
+sayHi();
+const anonymouFunction = () => {
+  'yes';
+};
+anonymouFunction(people);
